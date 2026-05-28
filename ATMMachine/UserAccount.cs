7b282c9f507b8ffe722844778ce0aa4753e6a91c
Asverktus <aleksandr.rubin123@gmail.com>
2026-05-28 18:32:38 +0700

    Пуш файлов разработанного приложения - смуляция банкомата
    В этой версии приложения пользователь может вносить, снимать и переводить деньги между пользователями в своем счете.
    
    При запуске пользователь вводит логин и пароль.
    Если пароль неверный, спустя 3 неудачной попытки программа завершает свою работу.

---
+++ ATMMachine/UserAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace AtmMachine
+{
+  public class UserAccount
+  {
+    public string Username { get; private set; }
+    private string _passwordHash;
+    public decimal Balance { get; set; }
+
+    public UserAccount(string username, string password, decimal initialBalance)
+    {
+      Username = username;
+      _passwordHash = password;
+      Balance = initialBalance;
+    }
+
+    public bool ValidatePassword(string password)
+    {
+      return _passwordHash == password;
+    }
+
+    public string GetPasswordHash()
+    {
+      return _passwordHash;
+    }
+  }
+}
